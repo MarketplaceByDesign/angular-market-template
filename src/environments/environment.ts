8399790f229e90e4d5af12d939e34bc2142bdcf7
github-ntalpada <105642867+github-ntalpada@users.noreply.github.com>
2025-05-07 15:07:40 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://us-east-2-prod-capapi.marketplacebydesign.net/',
+    apiUrl: 'https://us-west-2-dev-capapi.marketplacebydesign.net/',
     marketplaceName: 'Marketplace by Design',
     enableProxy: false,
     enableCMS: true,
